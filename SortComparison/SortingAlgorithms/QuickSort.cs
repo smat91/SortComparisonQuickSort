c708700032238f1aaeb6eccabc921891d4e43cdd
Matthias Stieger <matthiass413@gmail.com>
2021-12-15 18:30:02 +0100

    Add project files.

---
+++ SortComparison/SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SortComparison.SortingAlgorithms
+{
+    class QuickSort : SortAlgorithm
+    {
+        public override string Name => "Quicksort";
+        public override void Sort(IList<int> arrayToSort)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
